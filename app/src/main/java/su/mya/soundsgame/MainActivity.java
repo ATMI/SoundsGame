c398aac6f23c45394a73f9c4c320daa2cf7e656c
artem <artem.myasoedov@gmail.com>
2022-04-04 18:05:19 +0300

    Bvaa

--- app/src/main/java/su/mya/soundsgame/MainActivity.java
+++ app/src/main/java/su/mya/soundsgame/MainActivity.java
@@ -1,13 +1,12 @@
 package su.mya.soundsgame;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Pair;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.HashMap;
 
@@ -23,36 +22,36 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
-	static final HashMap<Integer, Digit> digits = new HashMap<Integer, Digit>() {{
-		put(R.id.d0, new Digit(R.drawable.digit_0, "sound_0.ogg"));
-		put(R.id.d1, new Digit(R.drawable.digit_1, "sound_1.ogg"));
-		put(R.id.d2, new Digit(R.drawable.digit_2, "sound_2.ogg"));
-		put(R.id.d3, new Digit(R.drawable.digit_3, "sound_3.ogg"));
-		put(R.id.d4, new Digit(R.drawable.digit_4, "sound_4.ogg"));
-		put(R.id.d5, new Digit(R.drawable.digit_5, "sound_5.ogg"));
-		put(R.id.d6, new Digit(R.drawable.digit_6, "sound_6.ogg"));
-		put(R.id.d7, new Digit(R.drawable.digit_7, "sound_7.ogg"));
-		put(R.id.d8, new Digit(R.drawable.digit_8, "sound_8.ogg"));
-		put(R.id.d9, new Digit(R.drawable.digit_nine, "sound_nine.ogg"));
-	}};
+	static HashMap<Integer, Digit> digits = new HashMap<>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final ImageView digitView = findViewById(R.id.digit);
+		digits.put(R.id.d0, new Digit(R.drawable.digit_0, "sound_0.ogg"));
+		digits.put(R.id.d1, new Digit(R.drawable.digit_1, "sound_1.ogg"));
+		digits.put(R.id.d2, new Digit(R.drawable.digit_2, "sound_2.ogg"));
+		digits.put(R.id.d3, new Digit(R.drawable.digit_3, "sound_3.ogg"));
+		digits.put(R.id.d4, new Digit(R.drawable.digit_4, "sound_4.ogg"));
+		digits.put(R.id.d5, new Digit(R.drawable.digit_5, "sound_5.ogg"));
+		digits.put(R.id.d6, new Digit(R.drawable.digit_6, "sound_6.ogg"));
+		digits.put(R.id.d7, new Digit(R.drawable.digit_7, "sound_7.ogg"));
+		digits.put(R.id.d8, new Digit(R.drawable.digit_8, "sound_8.ogg"));
+		digits.put(R.id.d9, new Digit(R.drawable.digit_nine, "sound_nine.ogg"));
+
+		final ImageView display = findViewById(R.id.display);
 		final MediaPlayer player = new MediaPlayer();
+
 		final View.OnClickListener digitClick = new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				final Digit d = digits.get(view.getId());
+				if (d == null)
+					return;
 
 				try {
-					if (d == null)
-						return;
-
-					digitView.setImageResource(d.iconRes);
+					display.setImageResource(d.iconRes);
 					final AssetFileDescriptor fd = getAssets().openFd(d.soundRes);
 					player.reset();
 					player.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
