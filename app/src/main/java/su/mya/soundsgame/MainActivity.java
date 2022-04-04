d503adbc967a2e5400dfdc4126e15135c959fd97
artem <artem.myasoedov@gmail.com>
2022-04-04 17:53:36 +0300

    Simplified

--- app/src/main/java/su/mya/soundsgame/MainActivity.java
+++ app/src/main/java/su/mya/soundsgame/MainActivity.java
@@ -3,15 +3,38 @@ package su.mya.soundsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.io.IOException;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+	static class Digit {
+		public final int iconRes;
+		public final String soundRes;
+
+		Digit(int iconRes, String soundRes) {
+			this.iconRes = iconRes;
+			this.soundRes = soundRes;
+		}
+	}
+
+	static final HashMap<Integer, Digit> digits = new HashMap<Integer, Digit>() {{
+		put(R.id.d0, new Digit(R.drawable.digit_0, "sound_0.ogg"));
+		put(R.id.d1, new Digit(R.drawable.digit_1, "sound_1.ogg"));
+		put(R.id.d2, new Digit(R.drawable.digit_2, "sound_2.ogg"));
+		put(R.id.d3, new Digit(R.drawable.digit_3, "sound_3.ogg"));
+		put(R.id.d4, new Digit(R.drawable.digit_4, "sound_4.ogg"));
+		put(R.id.d5, new Digit(R.drawable.digit_5, "sound_5.ogg"));
+		put(R.id.d6, new Digit(R.drawable.digit_6, "sound_6.ogg"));
+		put(R.id.d7, new Digit(R.drawable.digit_7, "sound_7.ogg"));
+		put(R.id.d8, new Digit(R.drawable.digit_8, "sound_8.ogg"));
+		put(R.id.d9, new Digit(R.drawable.digit_nine, "sound_nine.ogg"));
+	}};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,54 +42,22 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		final ImageView digitView = findViewById(R.id.digit);
-
 		final MediaPlayer player = new MediaPlayer();
 		final View.OnClickListener digitClick = new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				int icon = R.drawable.digit_0;
-				String sound = "sound_";
-				final int id = view.getId();
-
-				if (R.id.d0 == id) {
-					sound += "0";
-					icon = R.drawable.digit_0;
-				} else if (R.id.d1 == id) {
-					sound += "1";
-					icon = R.drawable.digit_1;
-				} else if (R.id.d2 == id) {
-					sound += "2";
-					icon = R.drawable.digit_2;
-				} else if (R.id.d3 == id) {
-					sound += "3";
-					icon = R.drawable.digit_3;
-				} else if (R.id.d4 == id) {
-					sound += "4";
-					icon = R.drawable.digit_4;
-				} else if (R.id.d5 == id) {
-					sound += "5";
-					icon = R.drawable.digit_5;
-				} else if (R.id.d6 == id) {
-					sound += "6";
-					icon = R.drawable.digit_6;
-				} else if (R.id.d7 == id) {
-					sound += "7";
-					icon = R.drawable.digit_7;
-				} else if (R.id.d8 == id) {
-					sound += "8";
-					icon = R.drawable.digit_8;
-				} else if (R.id.d9 == id) {
-					sound += "nine";
-					icon = R.drawable.digit_nine;
-				}
+				final Digit d = digits.get(view.getId());
 
 				try {
-					digitView.setImageResource(icon);
-					final AssetFileDescriptor fd = getAssets().openFd(sound + ".ogg");
+					if (d == null)
+						return;
+
+					digitView.setImageResource(d.iconRes);
+					final AssetFileDescriptor fd = getAssets().openFd(d.soundRes);
 					player.reset();
 					player.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
 					player.prepare();
-					player.setVolume(1f, 1f);
+					// player.setVolume(1f, 1f);
 					player.start();
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -74,15 +65,8 @@ public class MainActivity extends AppCompatActivity {
 			}
 		};
 
-		findViewById(R.id.d0).setOnClickListener(digitClick);
-		findViewById(R.id.d1).setOnClickListener(digitClick);
-		findViewById(R.id.d2).setOnClickListener(digitClick);
-		findViewById(R.id.d3).setOnClickListener(digitClick);
-		findViewById(R.id.d4).setOnClickListener(digitClick);
-		findViewById(R.id.d5).setOnClickListener(digitClick);
-		findViewById(R.id.d6).setOnClickListener(digitClick);
-		findViewById(R.id.d7).setOnClickListener(digitClick);
-		findViewById(R.id.d8).setOnClickListener(digitClick);
-		findViewById(R.id.d9).setOnClickListener(digitClick);
+		for (Integer id : digits.keySet()) {
+			findViewById(id).setOnClickListener(digitClick);
+		}
 	}
 }
